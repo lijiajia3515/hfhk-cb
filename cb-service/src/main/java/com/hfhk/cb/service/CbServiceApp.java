a1ca8bc8381a61fb26656053e3fa8a24e5159598
lijiajia <lijiajia3515@163.com>
2021-01-26 10:51:43 +0800

    这个故事还没有完结。——亚索

--- cb-service/src/main/java/com/hfhk/cb/service/CbServiceApp.java
+++ cb-service/src/main/java/com/hfhk/cb/service/CbServiceApp.java
@@ -2,14 +2,12 @@ package com.hfhk.cb.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class ServiceCBApp {
+public class CbServiceApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceCBApp.class, args);
+		SpringApplication.run(CbServiceApp.class, args);
 	}
 
 }
